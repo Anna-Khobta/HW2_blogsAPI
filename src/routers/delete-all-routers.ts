647aca1881975a6046ea861aad68cf13c71602df
Anna-Khobta <ana14i88@gmail.com>
2023-02-12 01:26:27 +0300

    add delete-all-routers1

--- src/routers/delete-all-routers.ts
+++ src/routers/delete-all-routers.ts
@@ -3,8 +3,10 @@ import {blogs} from "./blogs-router";
 import {posts} from "./posts-router";
 
 export const deleteAllRouter = Router()
+
+let arr: any[]  = []
 deleteAllRouter.delete('/testing/all-data', (req: Request, res: Response ) => {
-    let arr: any[]  = []
+    arr = []
     res.send(204)
 })
 
