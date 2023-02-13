8266887edf97be5a81d307edfeea5a6d468a3f97
Anna-Khobta <ana14i88@gmail.com>
2023-02-13 16:33:01 +0300

    authorization undefind

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -10,20 +10,21 @@ export const authorizationMiddleware = ((req: Request, res:Response, next:NextFu
 
     // parse login and password from headers
 
-    if (!('Basic' === ((req.headers.authorization).split(' ')[0]))
-        || !(typeof req.headers.authorization === 'string') {
-        res.send(401)
-    } else {
+if (typeof req.headers.authorization !== "undefined") {
+    if ('Basic' === ((req.headers.authorization).split(' ')[0])) {
+
         const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
         const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
 
         // Verify login and password are set and correct
         if (login && password && login === auth.login && password === auth.password) {
+
             // Access granted...
             return next()
+        }
     }
+}
 
-    }
     res.send(401) // custom message
 
 })
